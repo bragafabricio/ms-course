93c7f4c3d92c1dafe2b207d23e9260159ce43e1a
Fabricio Braga <fabriciobrg@gmail.com>
2022-01-25 16:44:34 -0300

    Implemented hr-payroll with mock data

---
+++ hr-payroll/src/main/java/com/fabriciobraga/hrpayroll/services/PaymentService.java
@@ -0,0 +1,11 @@
+package com.fabriciobraga.hrpayroll.services;
+
+import com.fabriciobraga.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    public Payment getPayment (Long workerId, int days){
+        return new Payment("Bob", 200.0, days);
+    }
+}
